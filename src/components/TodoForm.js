2ca581087f4bc3d8a380116a52aa976115e84106
christianturner <christiandavidphoto@gmail.com>
2021-03-16 16:32:22 -0400

    list parent of form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
 
-function TodoForm() {
+function TodoForm(props) {
   const [input, setInput] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+
+    setInput(""); // when the submit button is clicked, we clear the input field
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
   };
 
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
-      <input type="text" placeholder="Add a Todo" value={input} name="text" />
+      <input
+        type="text"
+        placeholder="Add a Todo"
+        value={input}
+        name="text"
+        onChange={handleChange}
+      />
       <button className="todo-button"> Add Todo </button>
     </form>
   );
